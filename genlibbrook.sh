96b0aae5c6eb88ff76fd1dcf4cb84c104e6ba3e2
Francisco Ros <fjros@doalitic.com>
2016-03-08 12:33:06 +0100

    Add script for generating libbrook Python library

---
+++ genlibbrook.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -e
+
+./run-in-docker.sh mvn package
+./run-in-docker.sh generate \
+  -i https://api.brook.io/docs \
+  -l python \
+  -o samples/client/libbrook \
+  -c libbrook.config.json
