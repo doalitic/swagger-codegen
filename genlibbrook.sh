ad806d76f9690705b00ee4f3a50a5bf88ce237c6
Francisco Ros <fjros@doalitic.com>
2016-03-09 11:05:50 +0100

    Fix generation script
    swagger-codegen caches templates, so we need to rebuild the tool to make sure
    that all new changes are taken into account.

--- genlibbrook.sh
+++ genlibbrook.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+./run-in-docker.sh mvn clean
 ./run-in-docker.sh mvn package
 ./run-in-docker.sh generate \
   -i https://api.brook.io/docs \
   -l python \
   -o samples/client/libbrook \
   -c libbrook.config.json
+
