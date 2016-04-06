7b233e50c3a8780d563244439b0c5051492ea591
Francisco Ros <fjros@doalitic.com>
2016-04-06 18:41:11 +0200

    Add comments to genlibbrook.sh

--- genlibbrook.sh
+++ genlibbrook.sh
@@ -2,9 +2,17 @@
 
 set -e
 
+# Make sure api docs are generated
+#
 curl -o /dev/null -s https://api.brook.io/api-docs
+
+# Clean previous artifacts to force libbrook to be re-generated
+#
 ./run-in-docker.sh mvn clean
 ./run-in-docker.sh mvn package
+
+# Generate libbrook
+#
 ./run-in-docker.sh generate \
   -i https://api.brook.io/docs \
   -l python \
